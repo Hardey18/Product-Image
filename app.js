f94b6b7fcbdc12f31cee0580c9a6f7e6d4514ea9
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2020-07-13 12:51:28 +0100

    side nav display on mobile

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
+const sideNav = document.querySelector(".side-nav");
 hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("hamburger-cancel");
+    sideNav.classList.toggle("hidenav");
 })
